ba0e8c0970dc1b66ec7249a1b5ad514aabfcba44
MorskiDev <mdg12.mdg12@gmail.com>
2023-01-22 19:31:57 +0100

    Made the bot launchable

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
+const bot = new Discord.Client({intents: 3276799})
+const config = require('./config')
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.tag} is alive et allumé succesfully !`)
+})
+
+bot.login(config.token)
