7671fe6236ed8386f882a42074bde086caf01fe3
MorskiDev <103139159+MorskiDev@users.noreply.github.com>
2024-01-03 18:55:43 +0100

    Démarrage du projet Morski Bot

---
+++ main.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js")
+const bot = new Discord.Client({intents: 3276799})
+const config = require('./config')
+
+bot.login(config.token)
+bot.on('ready', async () => {
+  console.log(`${bot.user.tag} is alive et démarré succesfully !`)
+})
